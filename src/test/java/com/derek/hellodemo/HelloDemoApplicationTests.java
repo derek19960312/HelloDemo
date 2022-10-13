4207c4814bb0996524a69214582cda2e1d94c30b
derek.huang <derek19960312@gmail.com>
2022-10-13 12:57:18 +0800

    first add

---
+++ src/test/java/com/derek/hellodemo/HelloDemoApplicationTests.java
@@ -0,0 +1,27 @@
+package com.derek.hellodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class HelloDemoApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void hello_ReturnHello() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello world!!!")); // pass
+    }
+
+}
